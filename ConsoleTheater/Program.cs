c94ae06efa174daa9e864886e24f0f96078c1df7
Michael Green <michael.green94@hotmail.com>
2018-09-25 10:28:37 -0600

    pushed with ignore

---
+++ ConsoleTheater/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleTheater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
